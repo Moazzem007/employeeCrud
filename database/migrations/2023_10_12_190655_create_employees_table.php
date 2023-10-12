791c9fe592adba43380469f4a2b94baafc6c1d83
Moazzem007 <19103143@iubat.edu>
2023-10-13 01:28:32 +0600

    Created models and migrations.
    In this section I have created an Employee model and a Department model and the necessary migration files to create the database tables.

--- database/migrations/2023_10_12_190655_create_employees_table.php
+++ database/migrations/2023_10_12_190655_create_employees_table.php
@@ -12,8 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('employees', function (Blueprint $table) {
+
             $table->id();
+            $table->unsignedBigInteger('department_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->date('hire_date');
+            $table->string('job_title');
+            $table->string('salary');
+            $table->integer('department_id');
             $table->timestamps();
+            $table->foreign('department_id')->references('id')->on('departments')->onDelete('cascade');
         });
     }
 
